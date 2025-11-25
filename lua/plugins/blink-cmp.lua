b06cf1f706de7e050df83a008f298caee2973278
Liang-Shih Lin <liang@baoge.dev>
2025-11-25 21:12:15 +0800

    Hopefully fixed blink

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -5,13 +5,21 @@ return {
     completion = {
       list = {
         selection = {
-          preselect = true,
+          preselect = false,
           auto_insert = true,
         },
       },
+      confirm_key = "<CR>",
+      disable_auto_lines = true,
     },
     keymap = {
-      preset = "enter",
+      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+
+      ["<CR>"] = { "select_and_accept", "fallback" },
+
+      ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
+      ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
     },
   },
 }
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,10 +1,10 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    formatters = { "jq", "yq", "biome-check" },
+    formatters = { "jq", "yq", "prettier", "prettierd", "biome-check" },
     formatters_by_ft = {
-      javascript = { "biome-check" },
-      typescript = { "biome-check" },
+      javascript = { "biome-check", "prettier", "prettierd" },
+      typescript = { "biome-check", "prettier", "prettierd" },
       json = { "jq" },
       yaml = { "yq" },
     },
